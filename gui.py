f8a7da1a088344008bf3e497fe4eadd79d3289cc
Bas Wijnen <wijnen@debian.org>
2011-06-27 14:53:23 +0200

    stuff

--- gui.py
+++ gui.py
@@ -33,6 +33,10 @@ class gui:
 				ret.children += (self.__element ('Label', {'value': ':' + c.tail.strip ()}, []),)
 		return ret
 	def __init__ (self, name = None, external = {}):
+		self.__event = {}
+		self.__get = {}
+		self.__set = {}
+		self.__settings = {}
 		if not name:
 			name = os.path.basename (sys.argv[0])
 		self.__name = name
@@ -44,22 +48,26 @@ class gui:
 		tree = self.__parse (root)
 
 		assert tree.tag == 'gtk'
+		while tree.children[0].tag == 'Set':
+			self.__settings[tree.children[0].attributes['name']] = tree.children[0].attributes['value']
+			del tree.children[0]
 		self.__windows = []
 		assert len (tree.children) > 0
 		if tree.children[0].tag != 'Window':
 			tree = self.__element (tree.tag, {}, [self.__element ('Window', tree.attributes, tree.children)])
-		self.__event = {}
-		self.__get = {}
-		self.__set = {}
 		assert tree.attributes == {}
 		for w in tree.children:
 			assert w.tag == 'Window'
 			self.__windows += (self.__build (w),)
 		for w in self.__windows:
 			w.connect ('destroy', self.__destroy)
+		# Reverse order, so first defined window is shown last, therefore (most likely) on top
+		self.__windows.reverse ()
 		if self.__external != {}:
 			print 'Not all externally provided widgets were used: ' + str (self.__external)
 		del self.__external
+	def __getitem__ (self, key):
+		return self.__settings[key]
 	def __event_cb (self, object, name):
 		if self.__event[name][0] != None:
 			f = self.__event[name][0]
@@ -67,16 +75,26 @@ class gui:
 				f (self.__event[name][1])
 			else:
 				f ()
-	def __add_event (self, widget, event, desc, name = None):
+	def __add_event (self, desc, event, widget, name = None):
 		if name == None:
 			name = event
 		if name not in desc.attributes:
 			return
 		value = desc.attributes[name]
 		del desc.attributes[name]
-		assert value not in self.__get and value not in self.__set and value not in self.__event
-		self.__event[value] = [None, None]
+		assert value not in self.__get and value not in self.__set
+		if value not in self.__event:
+			self.__event[value] = [None, None]
 		widget.connect (event, self.__event_cb, value)
+	def __add_custom_event (self, desc, name):
+		if name not in desc.attributes:
+			return None
+		value = desc.attributes[name]
+		del desc.attributes[name]
+		assert value not in self.__get and value not in self.__set
+		if value not in self.__event:
+			self.__event[value] = [None, None]
+		return value
 	def __add_get (self, desc, name, cb, arg):
 		if name not in desc.attributes:
 			return
@@ -103,6 +121,8 @@ class gui:
 		assert value not in self.__get and value not in self.__set and value not in self.__event
 		self.__set[value] = (cb, arg)
 	def __getattr__ (self, name, arg = None):
+		if name not in self.__get:
+			print name
 		assert name in self.__get
 		if self.__get[name][0] != None:
 			return self.__get[name][0] (self.__get[name][1], arg)
@@ -130,10 +150,107 @@ class gui:
 		assert len (desc.children) == 1
 		child = self.__build (desc.children[0])
 		parent.add (child)
+	def __as_bool (self, value):
+		if type (value) == str:
+			assert value == 'True' or value == 'False'
+			return value == 'True'
+		return bool (value)
 	def __build_pack (self, desc, parent):
+		def expand (value, widget):
+			widget.set_data ('expand', self.__as_bool (value))
+			parent = widget.get_parent ()
+			if parent == None:
+				return
+			widget.set_child_packing (parent, widget.get_data ('expand'), widget.get_data ('fill'), 0, gtk.PACK_START)
+		def fill (value, widget):
+			widget.set_data ('fill', self.__as_bool (value))
+			parent = widget.get_parent ()
+			if parent == None:
+				return
+			widget.set_child_packing (parent, widget.get_data ('expand'), widget.get_data ('fill'), 0, gtk.PACK_START)
 		for c in desc.children:
-			parent.pack_start (self.__build (c))
-	def __build (self, desc):
+			x = self.__build (c, {'expand': expand, 'fill': fill})
+			if x.get_data ('expand') == None:
+				x.set_data ('expand', True)
+			if x.get_data ('fill') == None:
+				x.set_data ('fill', True)
+			parent.pack_start (x, x.get_data ('expand'), x.get_data ('fill'))
+	def __build_attach (self, desc, parent):
+		def parse (value):
+			w = value.split (',')
+			v = 0
+			if '' in w:
+				del w[w.index ('')]
+			if 'expand' in w:
+				v |= gtk.EXPAND
+				del w[w.index ('expand')]
+			if 'fill' in w:
+				v |= gtk.FILL
+				del w[w.index ('fill')]
+			if 'shrink' in w:
+				v |= gtk.SHRINK
+				del w[w.index ('shrink')]
+			assert w == []
+			return v
+		def xset (value, widget):
+			widget.set_data ('xopts', parse (value))
+			parent = widget.get_parent ()
+			if parent == None:
+				return
+			parent.child_set_property (widget, 'x-options', widget.get_data ('xopts'))
+		def yset (value, widget):
+			widget.set_data ('yopts', parse (value))
+			parent = widget.get_parent ()
+			if parent == None:
+				return
+			parent.child_set_property (widget, 'y-options', widget.get_data ('yopts'))
+		def lset (value, widget):
+			widget.set_data ('left', int (value))
+			parent = widget.get_parent ()
+			if parent == None:
+				return
+			parent.child_set_property (widget, 'left-attach', widget.get_data ('left'))
+		def rset (value, widget):
+			widget.set_data ('right', int (value))
+			parent = widget.get_parent ()
+			if parent == None:
+				return
+			parent.child_set_property (widget, 'right-attach', widget.get_data ('right'))
+		def tset (value, widget):
+			widget.set_data ('top', int (value))
+			parent = widget.get_parent ()
+			if parent == None:
+				return
+			parent.child_set_property (widget, 'top-attach', widget.get_data ('top'))
+		def bset (value, widget):
+			widget.set_data ('bottom', int (value))
+			parent = widget.get_parent ()
+			if parent == None:
+				return
+			parent.child_set_property (widget, 'bottom-attach', widget.get_data ('bottom'))
+		cols = parent.get_property ('n-columns')
+		current = [0, 0]
+		for c in desc.children:
+			x = self.__build (c, {'x-options': xset, 'y-options': yset, 'left': lset, 'right': rset, 'top': tset, 'bottom': bset})
+			if x.get_data ('xopts') == None:
+				x.set_data ('xopts', gtk.EXPAND | gtk.FILL)
+			if x.get_data ('yopts') == None:
+				x.set_data ('yopts', gtk.EXPAND | gtk.FILL)
+			if x.get_data ('left') == None:
+				x.set_data ('left', current[0])
+			if x.get_data ('right') == None:
+				x.set_data ('right', x.get_data ('left') + 1)
+			if x.get_data ('top') == None:
+				x.set_data ('top', current[1])
+			if x.get_data ('bottom') == None:
+				x.set_data ('bottom', x.get_data ('top') + 1)
+			current[0] = x.get_data ('right')
+			current[1] = x.get_data ('top')
+			if current[0] >= cols:
+				current[0] = 0
+				current[1] += 1
+			parent.attach (x, x.get_data ('left'), x.get_data ('right'), x.get_data ('top'), x.get_data ('bottom'), x.get_data ('xopts'), x.get_data ('yopts'))
+	def __build (self, desc, fromparent = None):
 		if desc.tag == 'Window':
 			ret = gtk.Window ()
 			def set_title (value, window):
@@ -154,15 +271,18 @@ class gui:
 		elif desc.tag == 'Button':
 			assert len (desc.children) == 1
 			ret = gtk.Button ()
-			self.__add_event (ret, 'clicked', desc)
+			self.__add_event (desc, 'clicked', ret)
 			self.__build_add (desc, ret)
 		elif desc.tag == 'CheckButton':
 			assert len (desc.children) == 1
 			ret = gtk.CheckButton ()
 			def get (checkbutton, junk):
 				return checkbutton.get_active ()
+			def set (value, checkbutton):
+				checkbutton.set_active (self.__as_bool (value))
 			self.__add_get (desc, 'value', get, ret)
-			self.__add_event (ret, 'toggled', desc)
+			self.__add_set (desc, 'set', set, ret)
+			self.__add_event (desc, 'toggled', ret)
 			self.__build_add (desc, ret)
 		elif desc.tag == 'Entry':
 			assert len (desc.children) == 0
@@ -173,8 +293,92 @@ class gui:
 				return entry.get_text ()
 			self.__add_set (desc, 'set', set, ret)
 			self.__add_get (desc, 'value', get, ret)
-			self.__add_event (ret, 'activate', desc)
-			self.__add_event (ret, 'changed', desc)
+			self.__add_event (desc, 'activate', ret)
+			self.__add_event (desc, 'changed', ret)
+		elif desc.tag == 'Frame':
+			assert len (desc.children) == 1
+			ret = gtk.Frame ()
+			def set (value, frame):
+				if value == '':
+					frame.set_label (None)
+				else:
+					frame.set_label (value)
+			self.__add_set (desc, 'label', set, ret)
+			self.__build_add (desc, ret)
+		elif desc.tag == 'Table':
+			if 'columns' in desc.attributes:
+				cols = int (desc.attributes['columns'])
+				del desc.attributes['columns']
+			else:
+				cols = 1
+			ret = gtk.Table (1, cols)
+			self.__build_attach (desc, ret)
+		elif desc.tag == 'FileChooserButton':
+			ret = gtk.FileChooserButton ('')
+			def title (value, button):
+				button.set_title (value)
+			def get (button, junk):
+				return button.get_filename ()
+			self.__add_set (desc, 'title', title, ret)
+			self.__add_get (desc, 'value', get, ret)
+		elif desc.tag == 'SpinButton':
+			ret = gtk.SpinButton ()
+			def setrange (value, button):
+				min, max = value.split (',')
+				button.set_range (float (min), float (max))
+			def get (button, junk):
+				return button.get_value ()
+			def set (value, button):
+				return button.set_value (float (value))
+			def increment (value, button):
+				return button.set_increments (*[float (x) for x in value.split (',')])
+			ret.set_increments (1, 10)
+			self.__add_set (desc, 'range', setrange, ret)
+			self.__add_set (desc, 'set', set, ret)
+			self.__add_set (desc, 'increment', increment, ret)
+			self.__add_get (desc, 'value', get, ret)
+			self.__add_event (desc, 'value-changed', ret)
+		elif desc.tag == 'ComboBoxText' or desc.tag == 'ComboBoxEntryText':
+			if desc.tag == 'ComboBoxText':
+				ret = gtk.combo_box_new_text ()
+			else:
+				ret = gtk.combo_box_entry_new_text ()
+			def setcontent (value, box):
+				if type (value) == str:
+					l = value.split ('\n')
+				else:
+					l = value
+				while box.get_model ().get_iter_first () != None:
+					box.remove_text (0)
+				for i in l:
+					box.append_text (i.strip ())
+			def get (box, junk):
+				return box.get_active_text ()
+			def set (value, box):
+				def fill (model, path, iter, d):
+					d += (model.get_value (iter, 0),)
+					return False
+				d = []
+				box.get_model ().foreach (fill, d)
+				if value in d:
+					box.set_active (d.index (value))
+				else:
+					box.append_text (value)
+					box.set_active (len (d))
+			if len (desc.children) > 0:
+				assert len (desc.children) == 1 and desc.children[0].tag == 'Label'
+				setcontent (desc.children[0].attributes['value'].split (':', 1)[1], ret)
+				ret.set_active (0)
+			self.__add_set (desc, 'content', setcontent, ret)
+			self.__add_set (desc, 'set', set, ret)
+			self.__add_get (desc, 'value', get, ret)
+			self.__add_event (desc, 'changed', ret)
+			if desc.tag == 'ComboBoxEntryText':
+				v = self.__add_custom_event (desc, 'activate')
+				if v != None:
+					ret.child.connect ('activate', self.__event_cb, v)
+		elif desc.tag == 'HSeparator':
+			ret = gtk.HSeparator ()
 		elif desc.tag == 'External':
 			assert len (desc.children) == 0
 			ret = self.__external[desc.attributes['id']]
@@ -182,6 +386,9 @@ class gui:
 			del desc.attributes['id']
 		else:
 			raise AssertionError ('invalid tag ' + desc.tag)
+		if fromparent != None:
+			for k in fromparent:
+				self.__add_set (desc, k, fromparent[k], ret)
 		if desc.attributes != {}:
 			raise AssertionError ('unused attributes for ' + desc.tag + ': ' + str (desc.attributes))
 		return ret
